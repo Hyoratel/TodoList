efbf23558ac90441db541acc66fdad0c2b06a439
Hyoratel <wjsgyqls95@gmail.com>
2025-03-31 15:59:54 +0900

    이벤트 추가

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,6 +1,27 @@
-<template></template>
+<template>
+  <li
+    class="List-group-item"
+    :class="{
+      'list-group-item-success': WebTransportBidirectionalStream.completed,
+    }"
+    @click="$emit('toggle-completed', todoItem.id)"
+  >
+    <span class="pointer" :class="{ 'todo-done': todoItem.completed }"
+      >{{ todoItem.todo }}{{ todoItem.completed ? '(완료)' : '' }}</span
+    >
+    <span
+      class="float-end badge bg-secondary pointer"
+      @click.stop="$emit('delete-todo', todoItem)"
+      >삭제</span
+    >
+  </li>
+</template>
 <script>
 export default {
-  name: 'TodoListItem', // 각각 InputTodo, TodoList, TodoListItem
+  name: 'TodoListItem',
+  props: {
+    todoItem: { type: Object, required: true },
+  },
+  emits: ['delete-todo', 'toggle-completed'],
 };
 </script>
