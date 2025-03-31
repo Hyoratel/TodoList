eedf39ec1310115cfa295953b348435779392b75
Hyoratel <wjsgyqls95@gmail.com>
2025-03-31 09:37:21 +0900

    개별 할 일 표시 뼈대

---
+++ src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<template></template>
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: { TodoListItem },
+  props: {
+    todoList: { type: Array, required: true },
+  },
+};
+</script>
